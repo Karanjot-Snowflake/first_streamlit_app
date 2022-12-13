6814e64439cade4077ef91f2fbe526adf1e34d69
Karanjot-Snowflake <120294585+Karanjot-Snowflake@users.noreply.github.com>
2022-12-13 15:43:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #Normalizes data into pandas so data can be prepared to be displayed
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays results in datafram
