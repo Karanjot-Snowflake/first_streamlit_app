6001cd5e9e569348e1b7784280c7cb32bad08412
Karanjot-Snowflake <120294585+Karanjot-Snowflake@users.noreply.github.com>
2022-12-13 15:44:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.write('The user entered ', fruit_choice)
 
 #Normalizes data into pandas so data can be prepared to be displayed
