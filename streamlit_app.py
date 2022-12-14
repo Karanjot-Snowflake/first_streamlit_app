8fde74289121c7b5353a40716e7901de2ac122c1
Karanjot-Snowflake <120294585+Karanjot-Snowflake@users.noreply.github.com>
2022-12-14 11:03:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+
 
 
 streamlit.title('My Parents New Heathly Diner')
@@ -37,6 +37,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays results in datafram
 streamlit.dataframe(fruityvice_normalized)
 
+requirements.txt
+snowflake-connector-python
+import snowflake.connector
 
 
 
