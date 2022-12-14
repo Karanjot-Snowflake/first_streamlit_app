e1bdcd08c7b978b24ad05ad345b8bdc35ffa0fff
Karanjot-Snowflake <120294585+Karanjot-Snowflake@users.noreply.github.com>
2022-12-14 15:25:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,6 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
-streamlit.stop()
 
 #ask user input for adding fruit
 def insert_row_snowflake(new_fruit):
